77e1c7bba947f670df4a7d1f7606a48a8240162f
BlancaMunizaga <bmunizaga1@uc.cl>
2021-06-13 01:50:03 -0400

    datos en sheet pero falta cambiar formato...

--- sript.py
+++ sript.py
@@ -4,49 +4,8 @@ from xml.etree import ElementTree
 import gspread
 from gspread_dataframe import set_with_dataframe
 
-
-chile = 'CHL'
-australia = 'AUS'
-colombia = 'COL'
-estados_unidos = 'USA'
-canada = 'CAN'
-costa_rica ='CRI'
-
-chile_info = requests.get('http://tarea-4.2021-1.tallerdeintegracion.cl/gho_CHL.xml')
-australia_info = requests.get('http://tarea-4.2021-1.tallerdeintegracion.cl/gho_AUS.xml')
-colombia_info = requests.get('http://tarea-4.2021-1.tallerdeintegracion.cl/gho_COL.xml')
-estados_unidos_info = requests.get('http://tarea-4.2021-1.tallerdeintegracion.cl/gho_USA.xml')
-canada_info = requests.get('http://tarea-4.2021-1.tallerdeintegracion.cl/gho_CAN.xml')
-costa_rica_info = requests.get('http://tarea-4.2021-1.tallerdeintegracion.cl/gho_CRI.xml')
-
-chile_excel_root = ElementTree.fromstring(chile_info.content)
-australia_excel_root = ElementTree.fromstring(australia_info.content)
-colombia_excel_root = ElementTree.fromstring(colombia_info.content)
-estados_unidos_excel_root = ElementTree.fromstring(estados_unidos_info.content)
-canada_excel_root = ElementTree.fromstring(canada_info.content)
-costa_rica_root = ElementTree.fromstring(costa_rica.content)
-
-
-def parse_XML(root, indicadores):
-    Fact = root.findall('Fact')
-    df_cols = ['GHO', 'COUNTRY', 'SEX', 'YEAR', 'GHECAUSES', 'AGEGROUP', 'Display', 'Numeric', 'Low', 'High']
-    rows = []
-    for node in Fact:
-        res = []
-        nombre = node.find(df_cols[0]).text
-        if nombre in indicadores:
-            res.append(nombre)
-            for el in df_cols[1:]: 
-                if node is not None and node.find(el) is not None:
-                    res.append(node.find(el).text)
-                else: 
-                    res.append(None)
-            rows.append({df_cols[i]: res[i] for i, _ in enumerate(df_cols)})
-            out_df = pd.DataFrame(rows, columns=df_cols)
-    return out_df
-
-Fact = chile_excel_root.findall('Fact')
-# def parse_XML(xml_file, df_cols): 
+# https://medium.com/@robertopreste/from-xml-to-pandas-dataframes-9292980b1c1c 
+# def parse_XML(xml_file, df_cols):  
 #     """Parse the input XML file and store the result in a pandas 
 #     DataFrame with the given columns. 
     
@@ -75,6 +34,14 @@ Fact = chile_excel_root.findall('Fact')
         
 #     return out_df
 
+
+chile = 'CHL'
+australia = 'AUS'
+colombia = 'COL'
+estados_unidos = 'USA'
+canada = 'CAN'
+costa_rica ='CRI'
+
 indicadores = ['Number of deaths', 'Number of infant deaths', 'Number of under-five deaths',
 'Mortality rate for 5-14 year-olds (probability of dying per 1000 children aged 5-14 years)','Adult mortality rate (probability of dying between 15 and 60 years per 1000 population)',
 'Estimates of number of homicides','Crude suicide rates (per 100 000 population)','Mortality rate attributed to unintentional poisoning (per 100 000 population)',
@@ -96,54 +63,90 @@ indicadores_peso =['Mean BMI (kg/m&#xb2;) (crude estimate)','Mean BMI (kg/m&#xb2
 otros_indicadores = ['Alcohol, recorded per capita (15+) consumption (in litres of pure alcohol)', 'Estimate of daily cigarette smoking prevalence (%)', 'Estimate of daily tobacco smoking prevalence (%)', 'Estimate of current cigarette smoking prevalence (%)', 
 'Estimate of current tobacco smoking prevalence (%)', 'Mean systolic blood pressure (crude estimate)','Mean fasting blood glucose (mmol/l) (crude estimate)', 'Mean Total Cholesterol (crude estimate)']
 
-df_cols = ['GHO', 'COUNTRY', 'SEX', 'YEAR', 'GHECAUSES', 'AGEGROUP', 'Display', 'Numeric', 'Low', 'High']
+chile_info = requests.get('http://tarea-4.2021-1.tallerdeintegracion.cl/gho_CHL.xml')
+australia_info = requests.get('http://tarea-4.2021-1.tallerdeintegracion.cl/gho_AUS.xml')
+colombia_info = requests.get('http://tarea-4.2021-1.tallerdeintegracion.cl/gho_COL.xml')
+estados_unidos_info = requests.get('http://tarea-4.2021-1.tallerdeintegracion.cl/gho_USA.xml')
+canada_info = requests.get('http://tarea-4.2021-1.tallerdeintegracion.cl/gho_CAN.xml')
+costa_rica_info = requests.get('http://tarea-4.2021-1.tallerdeintegracion.cl/gho_CRI.xml')
 
-rows = []
-for node in Fact:
-    res = []
-    # aqui hacer if 
-    nombre = node.find(df_cols[0]).text
-    if nombre in indicadores:
-        res.append(nombre)
-        for el in df_cols[1:]: 
-            if node is not None and node.find(el) is not None:
-                res.append(node.find(el).text)
-            else: 
-                res.append(None)
-        rows.append({df_cols[i]: res[i] for i, _ in enumerate(df_cols)})
-        out_df = pd.DataFrame(rows, columns=df_cols)
-out_df.to_excel("algo.xlsx")
-    # GHO = node.find('GHO').text if node is not None else None
-    # COUNTRY = node.find('COUNTRY').text if node is not None else None
-    # SEX = node.find('SEX').text if node is not None else None
-    # YEAR = node.find('YEAR').text if node is not None else None
-    # GHECAUSES = node.find('GHECAUSES').text if node is not None else None
-    # AGEGROUP = node.find('AGEGROUP').text if node is not None else None
-    # Display = node.find('Display').text if node is not None else None
-    # Numeric = node.find('Numeric').text if node is not None else None
-    # Low = node.find('Low').text if node is not None else None
-    # High = node.find('High').text if node is not None else None
-    # print(GHO, COUNTRY, SEX, YEAR, GHECAUSES, AGEGROUP, Display)
-    # numbre_death = node.find("Number of deaths").text if node is not None else None
-    # if numbre_death is not None:
-    #     print(numbre_death)
-# print(aspectos)
-# chile_excel_root.iter('*'):
-# for child in chile_excel_root.iter(''):
-#     print(child.getchildren().attrib)
+chile_excel_root = ElementTree.fromstring(chile_info.content)
+australia_excel_root = ElementTree.fromstring(australia_info.content)
+colombia_excel_root = ElementTree.fromstring(colombia_info.content)
+estados_unidos_excel_root = ElementTree.fromstring(estados_unidos_info.content)
+canada_excel_root = ElementTree.fromstring(canada_info.content)
+costa_rica_root = ElementTree.fromstring(costa_rica_info.content)
+
+
+def parse_XML(root, indicadores):
+    Fact = root.findall('Fact')
+    df_cols = ['GHO', 'COUNTRY', 'SEX', 'YEAR', 'GHECAUSES', 'AGEGROUP', 'Display', 'Numeric', 'Low', 'High']
+    rows = []
+    for node in Fact:
+        res = []
+        nombre = node.find(df_cols[0]).text
+        if nombre in indicadores:
+            res.append(nombre)
+            for el in df_cols[1:]: 
+                if node is not None and node.find(el) is not None:
+                    res.append(node.find(el).text)
+                else: 
+                    res.append(None)
+            rows.append({df_cols[i]: res[i] for i, _ in enumerate(df_cols)})
+            out_df = pd.DataFrame(rows, columns=df_cols)
+    return out_df
+
+df_chile_muertes = parse_XML(chile_excel_root, indicadores_muertes)
+df_chile_muertes.to_excel("algo.xlsx")
+df_australia_muertes = parse_XML(australia_excel_root, indicadores_muertes)
+df_colombia_muertes = parse_XML(colombia_excel_root, indicadores_muertes)
+df_estados_unidos_muertes = parse_XML(estados_unidos_excel_root, indicadores_muertes)
+df_canada_muertes = parse_XML(canada_excel_root, indicadores_muertes)
+df_costa_rica_muertes = parse_XML(costa_rica_root, indicadores_muertes)
+
+df_muerte = pd.concat([df_chile_muertes, df_australia_muertes, df_colombia_muertes, df_estados_unidos_muertes, df_canada_muertes, df_costa_rica_muertes])
+
+df_chile_peso = parse_XML(chile_excel_root, indicadores_peso)
+df_australia_peso = parse_XML(australia_excel_root, indicadores_peso)
+df_colombia_peso = parse_XML(colombia_excel_root, indicadores_peso)
+df_estados_unidos_peso = parse_XML(estados_unidos_excel_root, indicadores_peso)
+df_canada_peso = parse_XML(canada_excel_root, indicadores_peso)
+df_costa_rica_peso = parse_XML(costa_rica_root, indicadores_peso)
+
+df_peso = pd.concat([df_chile_peso, df_australia_peso, df_colombia_peso, df_estados_unidos_peso, df_canada_peso, df_costa_rica_peso])
+
+df_chile_otros = parse_XML(chile_excel_root, otros_indicadores)
+df_australia_otros = parse_XML(australia_excel_root, otros_indicadores)
+df_colombia_otros = parse_XML(colombia_excel_root, otros_indicadores)
+df_estados_unidos_otros = parse_XML(estados_unidos_excel_root, otros_indicadores)
+df_canada_otros = parse_XML(canada_excel_root, otros_indicadores)
+df_costa_rica_otros = parse_XML(costa_rica_root, otros_indicadores)
+
+df_otros = pd.concat([df_chile_otros, df_australia_otros, df_colombia_otros, df_estados_unidos_otros, df_canada_otros, df_costa_rica_otros])
+
+
+
+
+# # out_df.to_excel("algo.xlsx")
+    
 print("hola")
 
 
 
-# # ACCES GOOGLE SHEET
-# gc = gspread.service_account(
-#     filename='taller-tarea-4-315516-d690cdb3da75.json')
-# # 'your_google_sheet_ID'
-# sh = gc.open_by_key('1FPVgjpA8FentUjEFL-Lnq5gVcL15Eogv8tk7SQ5qhBI')
-# # sheet_index_no -> 0 first sheet, 1 second  sheet
-# worksheet = sh.get_worksheet(0)
+# ACCES GOOGLE SHEET
+gc = gspread.service_account(
+    filename='taller-tarea-4-315516-d690cdb3da75.json')
+# 'your_google_sheet_ID'
+sh = gc.open_by_key('1FPVgjpA8FentUjEFL-Lnq5gVcL15Eogv8tk7SQ5qhBI')
+# sheet_index_no -> 0 first sheet, 1 second  sheet
+worksheet = sh.get_worksheet(0)
+worksheet2 = sh.get_worksheet(1)
+worksheet3 = sh.get_worksheet(2)
+
 
 # # APPEND DATA TO SHEET
 # your_dataframe = pd.DataFrame()
-# # ->This exports your dataframe to the googleSheet
-# set_with_dataframe(worksheet, your_dataframe)
+# ->This exports your dataframe to the googleSheet
+set_with_dataframe(worksheet, df_muerte)
+set_with_dataframe(worksheet2, df_peso)
+set_with_dataframe(worksheet3, df_otros)
